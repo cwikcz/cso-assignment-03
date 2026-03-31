ccfac9e13bd642006c46319085d77c85d75ac892
cwikcz <csw9868@nyu.edu>
2026-03-31 14:13:41 -0400

    Final

--- compareAges.s
+++ compareAges.s
@@ -4,16 +4,14 @@ compareAges:
     mov 4(%rsi), %edx #edx = b -> age
 
     cmp %edx, %eax #compare a -> age with b -> age
-    jl less_than #a < b
-    jg greater_than #a > b
-
-    mov $0, %eax #equal
+    jne not_equal # if not equal, return 0
+    
+    mov $1, %eax #equal, return 1
     ret
 
-less_than:
-    mov $-1, %eax
+not_equal:
+    mov $0, %eax
     ret
-greater_than:
-    mov $1, %eax
-    ret
+    
+
     
--- findPaymentsSum.s
+++ findPaymentsSum.s
@@ -1,17 +1,18 @@
     .globl findPaymentsSum
 findPaymentsSum:
     mov $0, %eax #sum = 0
-    mov $0, %ecx #i = 0
+    mov $0, %rcx #i = 0
 
 loop:
-    cmp $esi, %ecx #if i>= num, stop
+    cmp %esi, %ecx #if i>= num, stop
     jge done
 
-    mov 8(%rdi, %ecx, 4), %edx # edx = c -> payments[i]
+    mov 8(%rdi, %rcx, 4), %edx # edx = c -> payments[i]
     add %edx, %eax # sum += payments[i]
 
-    inc %ecx # i++
+    inc %rcx # i++
     jmp loop
 
 done:
     ret
+    
